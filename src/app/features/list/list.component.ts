13d98f034802ba562ee649237bec58e5ae00d876
arthurfoschiani <arthur.foschiani@outlook.com>
2024-11-10 18:12:24 -0300

    feat: Adiciona card para mostrar produto

--- src/app/features/list/list.component.ts
+++ src/app/features/list/list.component.ts
@@ -1,11 +1,13 @@
 import { Product } from '../../shared/interfaces/product.interface';
 import { ProductsService } from './../../shared/services/products.service';
 import { Component, inject } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './list.component.html',
   styleUrl: './list.component.scss'
 })
